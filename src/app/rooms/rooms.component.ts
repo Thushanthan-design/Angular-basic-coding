2da30f6b69c35d0cbca3fa90f1272592bd2c7a79
Thushanthan <mohanthushanthan2@gmail.com>
2023-06-11 21:46:59 +0530

    first commit

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { Room, RoomList } from './rooms';
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.css'],
+})
+export class RoomsComponent {
+  hotelName = 'Hilton Hotel';
+  numberOfRooms = false;
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5,
+  };
+
+  roomList: RoomList[] = [
+    {
+      roomNumber: 1,
+      roomType: 'Duluxe Rooms',
+      amenities: 'Air Condioner, Free Wi-Fi, Tv, Bathroom, Kitchen',
+      price: 500,
+      photo: 'https://pixabay.com/photos/lobby-hotel-interior-hallway-398845/',
+      checkinTime: new Date('11-nov-2023'),
+      checkoutTime: new Date('12-nov-2023'),
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Duluxe Rooms',
+      amenities: 'Air Condioner, Free Wi-Fi, Tv, Bathroom, Kitchen',
+      price: 500,
+      photo: 'https://pixabay.com/photos/lobby-hotel-interior-hallway-398846/',
+      checkinTime: new Date('11-nov-2023'),
+      checkoutTime: new Date('12-nov-2023'),
+    },
+    {
+      roomNumber: 3,
+      roomType: 'Duluxe Rooms',
+      amenities: 'Air Condioner, Free Wi-Fi, Tv, Bathroom, Kitchen',
+      price: 1500,
+      photo: 'https://pixabay.com/photos/lobby-hotel-interior-hallway-398847/',
+      checkinTime: new Date('11-nov-2023'),
+      checkoutTime: new Date('12-nov-2023'),
+    },
+    {
+      roomNumber: 4,
+      roomType: 'Duluxe Rooms',
+      amenities: 'Air Condioner, Free Wi-Fi, Tv, Bathroom, Kitchen',
+      price: 5000,
+      photo: 'https://pixabay.com/photos/lobby-hotel-interior-hallway-398845/',
+      checkinTime: new Date('11-nov-2023'),
+      checkoutTime: new Date('12-nov-2023'),
+    },
+  ];
+}
